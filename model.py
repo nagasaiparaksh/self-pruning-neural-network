d5ea503098706ae93299657368f02b2bf524f30c
Naga Sai Paraksh <nagasaiparaksh@gmail.com>
2026-04-23 20:19:07 +0530

    Implement prunable linear layer and neural network

---
+++ model.py
@@ -0,0 +1,26 @@
+class PrunableLinear(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(out_features, in_features)*0.01)
+        self.bias = nn.Parameter(torch.zeros(out_features))
+        self.gate_scores = nn.Parameter(torch.randn(out_features, in_features))
+
+    def forward(self, x):
+        gates = torch.sigmoid(self.gate_scores)
+        pruned_weights = self.weight * gates
+        return F.linear(x, pruned_weights, self.bias)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = PrunableLinear(3072, 512)
+        self.fc2 = PrunableLinear(512, 256)
+        self.fc3 = PrunableLinear(256, 10)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
